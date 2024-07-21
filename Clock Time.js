f768c04dde2a7a0fd5a156e8ae27d74b493eb845
simoneMoreWare <candidosimone598@gmail.com>
2024-07-21 19:29:03 +0200

    add clouds

--- Clock Time.js
+++ Clock Time.js
@@ -1,28 +1,41 @@
-let background;
 let factorScale = 1;
+let background;
 let dimXBackground = 1600;
 let dimYBackground = 800;
+let cloud;
+let dimXCloud = 140;
+let dimYCloud = 100;
+let posXCloud1 = 1200;
+let posYCloud1 = 100;
+let posXCloud2 = 760;
+let posYCloud2 = 150;
 
 function preload(){
+
     background = loadImage('background.png');
+	cloud = loadImage('cloud.png');
 }
 
 function setup(){
 
+	// Calculate scale factor to adapt image of display dimensions
 	factorScale = min(displayWidth / dimXBackground, displayHeight / dimYBackground)*1;
 
     createCanvas(dimXBackground * factorScale, dimYBackground * factorScale);
 
-    // Calcola il fattore di scala per adattare l'immagine alle dimensioni dello schermo
-
-    console.log(factorScale);
-
-    // Ridimensiona l'immagine mantenendo le proporzioni
+    // resize images to keep same proportions
     background.resize(dimXBackground * factorScale, dimYBackground * factorScale);
+	cloud.resize(dimXCloud * factorScale, dimYCloud * factorScale);
 
 }
 
 function draw(){
-    // Disegna l'immagine sul canvas
+
+    // Draw background on canvas
 	image(background, 0, 0);
+
+	//Draw clouds
+	image(cloud, posXCloud1 * factorScale, posYCloud1 * factorScale);
+	image(cloud, posXCloud2 * factorScale, posYCloud2 * factorScale);
+
 }
